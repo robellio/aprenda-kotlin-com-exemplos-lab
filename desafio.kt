dd2b8b14fb6daa89c9a1e77f6ce7408bf3a2c682
Robelio Barreto <robeliobasantos@gmail.com>
2023-06-10 22:19:08 -0300

    Resolução do desafio de projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    BASICO, INTERMEDIARIO, AVANCADO
+}
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome.toString()} matriculado na formação $nome $nivel")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   
+    val conteudo1 = ConteudoEducacional("Fundamentos da Linguagem", 120)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 180)
+    val conteudo3 = ConteudoEducacional("Programação Orientada a Objetos", 240)
+
+    val formacao = Formacao("Python Developer", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+
+    val usuario1 = Usuario("Thiago")
+    val usuario2 = Usuario("Jhonas")
+    val usuario3 = Usuario("Felipe")
+    val usuario4 = Usuario("Henrique")
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+    formacao.matricular(usuario4)
+    
+	println()
+    println("Usuários inscritos na formação:\n")
+    for (usuario in formacao.inscritos) {
+        println(usuario.nome)
+    }
 }
